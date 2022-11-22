02f8c890912550490e1973463f7539d8d5264f5e
irfankhan14 <irfankhan.ys@gmail.com>
2022-11-22 19:34:01 +0530

    Model HomePage Added

---
+++ Irfan Khan S/model/HomeItems.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Irfan Khan S
+//
+//  Created by Irfan Khan on 11/11/22.
+//
+
+class HomeItems {
+  
+  var id: Int
+  var name: String
+  var image: UIImage
+
+  init(idValue: Int, nameValue: String, imageValue: UIImage){
+    self.id = idValue
+    self.name = nameValue
+    self.image = imageValue
+  }
+
+}
