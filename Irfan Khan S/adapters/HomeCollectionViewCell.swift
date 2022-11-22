be6941d94c0f603d42f1debf17f10f14b6f32b82
irfankhan14 <irfankhan.ys@gmail.com>
2022-11-22 19:07:32 +0530

    Account Items added in home

---
+++ Irfan Khan S/adapters/HomeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeCollectionViewCell.swift
+//  Irfan Khan S
+//
+//  Created by Irfan Khan on 17/11/22.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var homeItemName: UILabel!
+    @IBOutlet weak var homeItemImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Irfan Khan S/base/views/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeViewController.swift
+//  Irfan Khan S
+//
+//  Created by Irfan Khan on 17/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
+    @IBOutlet weak var collectionAccounts: UICollectionView!
+    
+    var accountsList = Array<HomeItems>()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        initializeAccountsList()
+        
+        let nibcell = UINib(nibName: "HomeCollectionViewCell", bundle: nil)
+        collectionAccounts.register(nibcell, forCellWithReuseIdentifier: "accounts_cell")
+
+        
+    }
+    
+    private func initializeAccountsList() {
+        accountsList.removeAll()
+        
+        accountsList.append(HomeItems(idValue: 0, nameValue: NSLocalizedString("txt_sbi", comment: ""), imageValue: UIImage(named: "ic_home_sbi")!))
+        accountsList.append(HomeItems(idValue: 1, nameValue: NSLocalizedString("txt_icici", comment: ""), imageValue: UIImage(named: "ic_home_icici")!))
+        accountsList.append(HomeItems(idValue: 2, nameValue: NSLocalizedString("txt_icici_credit", comment: ""), imageValue: UIImage(named: "ic_home_credit_card")!))
+        accountsList.append(HomeItems(idValue: 3, nameValue: NSLocalizedString("txt_zerodha", comment: ""), imageValue: UIImage(named: "ic_home_zerodha")!))
+        accountsList.append(HomeItems(idValue: 4, nameValue: NSLocalizedString("txt_pf", comment: ""), imageValue: UIImage(named: "ic_home_pf")!))
+        
+        
+        accountsList.append(HomeItems(idValue: 5, nameValue: NSLocalizedString("txt_paytm", comment: ""), imageValue: UIImage(named: "ic_home_paytm")!))
+        accountsList.append(HomeItems(idValue: 6, nameValue: NSLocalizedString("txt_phonepe", comment: ""), imageValue: UIImage(named: "ic_home_phonepe")!))
+        accountsList.append(HomeItems(idValue: 7, nameValue: NSLocalizedString("txt_amazon_pay", comment: ""), imageValue: UIImage(named: "ic_home_amazon_pay")!))
+        accountsList.append(HomeItems(idValue: 8, nameValue: NSLocalizedString("txt_fast_tag", comment: ""), imageValue: UIImage(named: "ic_home_fast_tag")!))
+        accountsList.append(HomeItems(idValue: 9, nameValue: NSLocalizedString("txt_home", comment: ""), imageValue: UIImage(named: "ic_home_accounts_home")!))
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return accountsList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "accounts_cell", for: indexPath) as! HomeCollectionViewCell
+        cell.homeItemImage.image = accountsList[indexPath.row].image
+        cell.homeItemName.text = accountsList[indexPath.row].name
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let name = accountsList[indexPath.row].name
+        print("Item selected is:" + name)
+    }
+
+}
